672bfb89b81ddd7ecfbcdb35c1b751f0451405ea
Rodrigo Teixeira <rodrigo201101@hotmail.com>
2024-06-11 21:46:30 -0300

    Turn manager RPG

---
+++ dist/assets/default-BXa_iPat.js
@@ -0,0 +1,1 @@
+import{r as o,o as r,c as n,w as e,V as s,a as t,b as _}from"./index-Bm3ZvXh9.js";const f={__name:"default",setup(c){return(l,u)=>{const a=o("router-view");return r(),n(s,null,{default:e(()=>[t(_,null,{default:e(()=>[t(a)]),_:1})]),_:1})}}};export{f as default};
